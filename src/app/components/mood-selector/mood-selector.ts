7e4a022fb4100e22a38c969a1dd91f2198ac00c4
macbookpro <mahsinsaifullah@gmail.com>
2025-09-20 21:57:12 +0100

    feat: add select mood functionality

--- src/app/components/mood-selector/mood-selector.ts
+++ src/app/components/mood-selector/mood-selector.ts
@@ -1,13 +1,20 @@
 import { Component, inject } from '@angular/core';
 import { MoodService } from '../../services/mood.service';
+import { NgClass } from '@angular/common';
+import { Mood } from '../../models/mood.model';
 
 @Component({
   selector: 'app-mood-selector',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './mood-selector.html',
   styleUrl: './mood-selector.css',
 })
 export class MoodSelector {
   private moodService = inject(MoodService);
   allMoods = this.moodService.getAllMoods();
+  selectedMood = this.moodService.selectedMood;
+
+  onSelectMood(mood: Mood) {
+    this.moodService.selectMood(mood);
+  }
 }
