394f91ca07ad677342ec175d97abe1a9312c01dd
macbookpro <mahsinsaifullah@gmail.com>
2025-09-27 18:33:43 +0100

    feat: add fetchPopularMovies method in movie api service

--- src/app/services/movie-api.service.ts
+++ src/app/services/movie-api.service.ts
@@ -2,12 +2,19 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Movie } from '../models/movie.model';
 
+interface MovieApiRequest {
+  results: Movie[];
+  page: number;
+  total_pages: number;
+  total_results: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class MovieApiService {
   private http = inject(HttpClient);
-  private readonly apiUrl = 'https://api.themoviedb.org/3/discover/movie';
+  private readonly baseUrl = 'https://api.themoviedb.org/3';
   private readonly apiToken =
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDZhNDI1M2Y3NGM2MWE3NjEwNGJiMzNkNzIxMzhiOCIsIm5iZiI6MTY2MTk3NDkxMy40OTUwMDAxLCJzdWIiOiI2MzBmYjk4MWQ3YTcwYTAwN2UxYmE5ODYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.J2ny-I48Ea2SnqN7SETJklWAjicA0T_ljJaCBFbFGBM';
 
@@ -21,7 +28,16 @@ export class MovieApiService {
       .set('with_genres', genreIds.join(','))
       .set('page', page.toString());
 
-    return this.http.get<{ results: Movie[] }>(this.apiUrl, {
+    return this.http.get<MovieApiRequest>(`${this.baseUrl}/discover/movie`, {
+      headers: this.defaultHeaders,
+      params,
+    });
+  }
+
+  fetchPopularMovies(page = 1) {
+    const params = new HttpParams().set('page', page.toString());
+
+    return this.http.get<MovieApiRequest>(`${this.baseUrl}/movie/popular`, {
       headers: this.defaultHeaders,
       params,
     });
